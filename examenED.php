68e9ed706aa27b76c29db80d2d760e4a07953b5f
Victor <victormanuelnavarrocamino@gmail.com>
2023-11-19 12:38:31 +0100

    Comienzo de examen

---
+++ examenED.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Examen ED</title>
+    <link rel="stylesheet" href="css/estilo.css">
+    <style>
+        div{
+            margin: auto;
+        }
+    </style>
+
+</head>
+<body>
+    <div>
+        <h1>NOMBRE COMPLETO DEL ALUMNO</h1>
+        <img src="img/nieve.jpg" alt="Foto de la nieve">
+    </div>
+</body>
+</html>
